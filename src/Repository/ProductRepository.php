278ac647f3001a3313ad1f646da5387bfcc21391
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-01 18:08:53 +0100

    feat: add find methods

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -6,7 +6,14 @@ use App\Document\Product;
 use Doctrine\Bundle\MongoDBBundle\Repository\ServiceDocumentRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-
+/**
+ * @extends ServiceDocumentRepository<Product>
+ *
+ * @method Product|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Product|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Product[]    findAll()
+ * @method Product[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
 class ProductRepository extends ServiceDocumentRepository
 {
     public function __construct(ManagerRegistry $registry)
