8257c07238da101e388d0b546ae435aaf6436080
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-20 21:44:08 +0100

    feat: setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = require("express");
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config(".env");
+
+const app = express();
+
+const port = process.env.MONGO_PORT || 3000;
+app.listen(port, () => {
+  // log server start timestamp
+});
