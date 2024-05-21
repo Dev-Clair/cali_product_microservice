11b5e7dd42df4b78a9b6f644c8e92ce9ff6bc7df
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-21 22:52:25 +0100

    fix: add comments

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const logger = require("./service/logger");
 const productqueue = require("./queue/productqueue");
 const productrouter = require("./route/productrouter");
 
+// Retrieve and Define Environment Variables
 dotenv.config(".env");
 
 const port = process.env.MONGO_PORT || 3000;
@@ -13,29 +14,27 @@ const rabbitmq_url = process.env.RABBITMQ_URL || "amqp//localhost:5672";
 
 const product_queue = "product_queue";
 
+// Create an Instance of the Express Application
 const app = express();
 
+// Define Middlewares
 app.use(express.json());
 
 app.use("api/v1/products", productrouter);
 
+// Start Server and Database Processes
 app.listen(port, () => {
-  // Start database
   try {
     mongoose
       .connect(process.env.MONGO_URI)
-      .then(
-        // Listen to queue in channel for messages
-        productqueue(rabbitmq_url, product_queue)
-      )
-      .catch();
+      .then(productqueue(rabbitmq_url, product_queue));
   } catch (error) {
     logger.log({
       level: "error",
       message: `${error.message}`,
     });
   }
-  // Log server start timestamp
+
   logger.log({
     level: "info",
     message: new Date().now(),
