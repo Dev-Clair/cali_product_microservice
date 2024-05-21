669eed909a21778160b577d9a6eeb0dbdc12f310
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-21 13:53:31 +0100

    feat: controller method definitions

---
+++ controller/productController.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const productmodel = require("./../model/productmodel");
+
+const app = express();
+
+/**
+ * Collection Operations
+ */
+exports.getProducts = (req, res) => {
+  // Retrieves product collection
+  productmodel;
+};
+
+exports.postProduct = (req, res) => {
+  // Creates a new product
+  productmodel;
+};
+
+/**
+ * Item Operations
+ */
+exports.getProduct = (req, res) => {
+  // Retrieves an existing product using its :id / :slug
+  productmodel;
+};
+
+exports.putProduct = (req, res) => {
+  // Modifies an existing product (entirely) using its :id / :slug
+  productmodel;
+};
+
+exports.patchProduct = (req, res) => {
+  // Modifies an existing product (partially) using its :id / :slug
+  productmodel;
+};
+
+exports.deleteProduct = (req, res) => {
+  // Removes an existing product using its :id / :slug
+  productmodel;
+};
