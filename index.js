c72fd0a6dababb8a125fc1dd9a7be10d92886407
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-21 22:01:38 +0100

    feat: add bodyparser, router

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const dotenv = require("dotenv");
 const express = require("express");
+const productrouter = require("./../route/productroute");
 
-dotenv.config(".env.local");
+dotenv.config(".env");
 
 const app = express();
 
+app.use(express.json());
+
+app.use("api/v1/products", productrouter);
+
 const port = process.env.MONGO_PORT || 3000;
+
 app.listen(port, () => {
   // log server start timestamp
 });
