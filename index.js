46ccce152898ba39d190e4f6b3925a044a126f78
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-22 13:47:25 +0100

    fix: rename router directory

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("./service/logger");
 const productqueue = require("./queue/productqueue");
-const productrouter = require("./route/productrouter");
+const productrouter = require("./router/productrouter");
 
 // Retrieve and Define Environment Variables
 dotenv.config(".env");
@@ -14,29 +14,30 @@ const rabbitmq_url = process.env.RABBITMQ_URL || "amqp//localhost:5672";
 
 const product_queue = "product_queue";
 
-// Create an Instance of the Express Application
+// Create an Express Application Instance
 const app = express();
 
 // Define Middlewares
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: false }));
+
 app.use("api/v1/products", productrouter);
 
-// Start Server and Database Processes
+// Start Server, Database and Queue Processes
 app.listen(port, () => {
   try {
     mongoose
-      .connect(process.env.MONGO_URI)
-      .then(productqueue(rabbitmq_url, product_queue));
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        productqueue(rabbitmq_url, product_queue);
+      });
   } catch (error) {
-    logger.log({
-      level: "error",
-      message: `${error.message}`,
-    });
+    logger.error({ message: `${error.message}` });
   }
 
-  logger.log({
-    level: "info",
-    message: new Date().now(),
-  });
+  logger.info({ message: Date.now() });
 });
---
+++ router/productrouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const productcontroller = require("../controller/productcontroller");
+
+const productrouter = express.Router();
+
+productrouter.route("/").get(productcontroller.getCollection);
+
+productrouter.route("/search?").get(productcontroller.getSearch);
+
+productrouter.route("/:id").get(productcontroller.get);
+
+module.exports = productrouter();
