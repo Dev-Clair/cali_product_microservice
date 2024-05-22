89e4a4d4ce117c63677a3d0f5058000625e58087
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-22 15:30:53 +0100

    feat: implement controller methods

--- controller/productController.js
+++ controller/productController.js
@@ -1,45 +1,136 @@
 const productmodel = require("./../model/productmodel");
+const logger = require("./../service/logger");
 
 /**
+ *
  * Collection Operations
+ *
  */
-exports.getCollection = (req, res) => {
+exports.getCollection = async (req, res) => {
   // Retrieves product collection
-  productmodel.find();
+  try {
+    const products = await productmodel.find();
+
+    res.status(200).json({
+      result: products.length(),
+      products: products,
+    });
+  } catch (error) {
+    logger.error("error", `${error.message}`);
+
+    res.status(500).json({
+      result: products.length(),
+      error: error,
+    });
+  }
 };
 
-exports.post = () => {
+exports.post = async (product) => {
   // Creates a new product
-  productmodel.create();
+  try {
+    const product = await productmodel.create(product);
+
+    logger.info(
+      `success | action: ${product.action} | product name: ${product.name} | product id: ${product.product_id}`
+    );
+  } catch (error) {
+    logger.error("error", `${error.message}`);
+  }
 };
 
 /**
+ *
  * Item Operations
+ *
  */
-exports.get = (req, res) => {
+exports.get = async (req, res) => {
   // Retrieves an existing product using its :id / :slug
-  productmodel.findOne();
+  try {
+    const product = await productmodel.findById();
+
+    res.status(200).json({
+      result: product.length,
+      product: product,
+    });
+  } catch (error) {
+    logger.error(`${error.message}`);
+
+    res.status(500).json({
+      result: product.length(),
+      error: error,
+    });
+  }
 };
 
-exports.put = () => {
+exports.put = async (product) => {
   // Modifies an existing product (entirely) using its :id / :slug
-  productmodel.findByIdAndUpdate();
+  try {
+    const product = await productmodel.findOneAndUpdate(
+      { product_id: product.product_id },
+      product,
+      { new: true }
+    );
+
+    logger.info(
+      `success | action: ${product.action} | product name: ${product.name} | product id: ${product.product_id}`
+    );
+  } catch (error) {
+    logger.error("error", `${error.message}`);
+  }
 };
 
-exports.patch = () => {
+exports.patch = async (product) => {
   // Modifies an existing product (partially) using its :id / :slug
-  productmodel.findByIdAndUpdate();
+  try {
+    const product = await productmodel.findOneAndUpdate(
+      { product_id: product.product_id },
+      product,
+      { new: true }
+    );
+
+    logger.info(
+      `success | action: ${product.action} | product name: ${product.name} | product id: ${product.product_id}`
+    );
+  } catch (error) {
+    logger.error("error", `${error.message}`);
+  }
 };
 
-exports.delete = () => {
+exports.delete = async (product) => {
   // Removes an existing product using its :id / :slug
-  productmodel.findByIdAndDelete();
+  try {
+    const product = await productmodel.findOneAndDelete({
+      product_id: product.product_id,
+    });
+
+    logger.info(
+      `success | action: ${product.action} | product name: ${product.name} | product id: ${product.product_id}`
+    );
+  } catch (error) {
+    logger.error("error", `${error.message}`);
+  }
 };
 
 /**
+ *
  * Hybrid Operations
+ *
  */
-exports.search = (req, res) => {
+exports.getSearch = async (req, res) => {
   // Retrieves an existing or collection of products based on search parameter
-  productmodel.find();
+  try {
+    const products = await productmodel.find();
+
+    res.status(200).json({
+      result: products.length,
+      products: products,
+    });
+  } catch (error) {
+    logger.error("error", `${error.message}`);
+
+    res.status(500).json({
+      result: products.length,
+      error: error,
+    });
+  }
 };
