edb1a0a75049048085f6c13b62ab3ae6ed41d58e
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-24 10:17:31 +0100

    fix: add virtual property

--- models/productmodel.js
+++ models/productmodel.js
@@ -1,65 +1,75 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 
-const productSchema = new mongoose.Schema({
-  product_id: {
-    type: Number,
-    required: [true, "A product must have a product ID"],
+const productSchema = new mongoose.Schema(
+  {
+    // product_id: {
+    //   type: String,
+    //   required: [true, "A product must have a product ID"],
+    // },
+    product_name: {
+      type: String,
+      trim: true,
+      required: [true, "A product must have a name"],
+    },
+    product_description: {
+      type: String,
+      trim: true,
+      required: [true, "A product must have a description"],
+    },
+    product_slug: {
+      type: String,
+      unique: true,
+    },
+    product_price: {
+      type: mongoose.Decimal128,
+      required: [true, "A product must have a price"],
+    },
+    product_stock_quantity: {
+      type: Number,
+      required: [true, "A product must have a stock_quantity"],
+    },
+    product_warranty: {
+      type: String,
+      trim: true,
+    },
+    product_colors: [String],
+    product_sizes: [String],
+    product_weight: {
+      type: String,
+      trim: true,
+    },
+    product_image_path: {
+      type: String,
+      trim: true,
+    },
+    product_category: {
+      type: String,
+      trim: true,
+      required: [true, "A product must have a category"],
+    },
   },
-  product_name: {
-    type: String,
-    trim: true,
-    required: [true, "A product must have a name"],
-  },
-  product_description: {
-    type: String,
-    trim: true,
-    required: [true, "A product must have a description"],
-  },
-  product_slug: {
-    type: String,
-    unique: true,
-    required: [true, "A product must have a slug"],
-  },
-  product_price: {
-    type: mongoose.Decimal128,
-    required: [true, "A product must have a price"],
-  },
-  product_stock_quantity: {
-    type: Number,
-    required: [true, "A product must have a stock_quantity"],
-  },
-  product_status: {
-    type: String,
-    trim: true,
-    required: [true, "A product must have a status"],
-  },
-  product_warranty: {
-    type: String,
-    trim: true,
-  },
-  product_colors: [String],
-  product_sizes: [String],
-  product_weight: {
-    type: String,
-    trim: true,
-  },
-  product_image_path: {
-    type: String,
-    trim: true,
-  },
-  product_category: {
-    type: String,
-    trim: true,
-    required: [true, "A product must have a category"],
-  },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-productSchema.pre("validate", function (next) {
+// Document Middleware
+productSchema.pre("save", function (next) {
   if (this.name) {
     this.slug = slugify(this.name, { lower: true, strict: true });
   }
   next();
+});
+
+// Virtual Properties
+productSchema.virtual("product_status").get(function () {
+  if (this.product_stock_quantity > 0) {
+    return "AVAILABLE";
+  }
+
+  return "OUT-OF-STOCK";
 });
 
 const Product = mongoose.model("Product", productSchema);
