78e28eeab75e20419c9d2511ad191b084c5a501a
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-24 07:33:44 +0100

    fix: server port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const logger = require("./service/loggerService");
 const productQueue = require("./queue/productQueue");
 const productRouter = require("./router/productRouter");
 
-// Retrieve Environment Variables
+// Load Environment Variables
 dotenv.config(".env");
 
 // Create an Express Application Instance
@@ -18,27 +18,29 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", productRouter.productRouter);
 
-// Start Queue Process
+// Start Server, Database and Queue Processes
+const port = process.env.PORT || 4000;
+
 const rabbitmq_url = process.env.RABBITMQ_URL || "amqp//localhost:5672";
 
 const product_exchange = "product_exchange";
 
 const product_queue = "product_queue";
 
-logger.info(`${product_queue} consumer started`);
-productQueue.productQueue(rabbitmq_url, product_exchange, product_queue);
-
-// Start Server and Database Processes
-const port = process.env.PORT || 4000;
-
 app.listen(port, async () => {
+  logger.info(
+    `Server process started: ${Date.now()} | Listening on port: ${port}`
+  );
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
     logger.info(`Connected to MongoDB`);
 
-    logger.info(
-      `Server process started: ${Date.now()} | Listening on port: ${port}`
+    await productQueue.productQueue(
+      rabbitmq_url,
+      product_exchange,
+      product_queue
     );
   } catch (error) {
     logger.error(`Error: ${error.message}`);
