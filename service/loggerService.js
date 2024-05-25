66167074cc8e31da8e75a5a8ca68d91c73f2ef49
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-25 18:46:15 +0100

    fix: reorder logger file

--- service/loggerService.js
+++ service/loggerService.js
@@ -14,8 +14,8 @@ const apiLogger = createLogger({
   ),
   transports: [
     new transports.Console(),
-    new transports.File({ filename: "./api/logs/combined.log" }),
     new transports.File({ filename: "./api/logs/error.log", level: "error" }),
+    new transports.File({ filename: "./api/logs/combined.log" }),
   ],
 });
 
@@ -27,8 +27,8 @@ const queueLogger = createLogger({
     logFormat
   ),
   transports: [
-    new transports.File({ filename: "./queue/logs/combined.log" }),
     new transports.File({ filename: "./queue/logs/error.log", level: "error" }),
+    new transports.File({ filename: "./queue/logs/combined.log" }),
   ],
 });
 
