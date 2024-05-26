a2fdaa8a8cae0a41a1155bd21a8a716e80001847
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-26 12:51:55 +0100

    feat: import database, logger and consumer scripts

--- cali-product-queue/handler.js
+++ cali-product-queue/handler.js
@@ -1,5 +1,18 @@
-exports.consumer = async (event) => {
+const dotenv = require("dotenv");
+const consumer = require("./src/consumer");
+const databaseService = require("./service/databaseService");
+const { queueLogger } = require("./service/loggerService");
+
+// Load Environment Variables
+dotenv.config(".env");
+
+exports.handle = async (event) => {
   for (const record of event.Records) {
-    console.log("Message Body: ", record.body);
+    await databaseService(process.env.MONGO_URI);
+    try {
+      await consumer(record.body);
+    } catch (error) {
+      queueLogger.error(`Error processing message: ${error.message}`);
+    }
   }
 };
