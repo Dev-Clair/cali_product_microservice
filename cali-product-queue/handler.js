1702cafdd1bc176945dff6be64ea49a8f19655d6
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-27 20:41:17 +0100

    fix: modify handler function

--- cali-product-queue/handler.js
+++ cali-product-queue/handler.js
@@ -2,11 +2,8 @@ const { consumer } = require("./src/consumer");
 
 exports.consume = async (event) => {
   for (const record of event.Records) {
-    try {
-      console.log(record.body);
-      await consumer(record.body);
-    } catch (error) {
-      console.log(`Error processing message: ${error.message}`);
-    }
+    await consumer(record.body).catch(() => {
+      console.log(`Process terminated: ${error.message}`);
+    });
   }
 };
