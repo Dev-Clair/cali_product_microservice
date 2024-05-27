48b3e975b2e9a5c6c41a48b11ed2f5a3e9129cab
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-27 12:22:53 +0100

    fix: handler method name

--- cali-product-queue/handler.js
+++ cali-product-queue/handler.js
@@ -1,7 +1,7 @@
 const { consumer } = require("./src/consumer");
 const { queueLogger } = require("./src/service/loggerService");
 
-exports.handle = async (event) => {
+exports.consume = async (event) => {
   for (const record of event.Records) {
     try {
       await consumer(record.body);
