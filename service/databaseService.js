0e2d62f26b0b8e77974c41b709bc9cc95087568c
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-05-28 10:50:42 +0100

    fix: remove logger and database connection file

--- service/databaseService.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const databaseService = async (connectionString) => {
-  await mongoose.connect(connectionString);
-};
-
-module.exports = { databaseService };
