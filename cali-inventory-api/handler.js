da723308990abee1fbb7e5ebefac96ce4720ad15
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-02 00:07:05 +0100

    fix: implement singleton pattern for database connection access

--- cali-inventory-api/handler.js
+++ cali-inventory-api/handler.js
@@ -1,27 +1,17 @@
 const serverless = require("serverless-http");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app");
+const { Connection } = require("./connection");
 
 process.on("unCaughtException", () => {
   console.warn("unCaughtException: Shutting down gracefully");
   process.exit(1);
 });
 
-// Load Environment Variables
 dotenv.config(".env");
 
-// Establish Database Connection
-const connectionString = process.env.MONGO_URI;
+const connection_uri = process.env.MONGO_URI;
 
-const databaseConnection = mongoose.connect(connectionString);
-
-databaseConnection
-  .then(() => {
-    console.info(`Database Connection Successful`);
-  })
-  .catch((error) => {
-    console.error(`Database Connection Unsuccessful: ${error.message}.`);
-  });
+Connection(connection_uri);
 
 exports.inventory = serverless(app);
