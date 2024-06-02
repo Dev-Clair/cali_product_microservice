3d50c0d08211697494c8c499a634b15e9ab52dca
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-02 16:31:10 +0100

    feat: implement singleton pattern for database connection resource

--- cali-product-api/handler.js
+++ cali-product-api/handler.js
@@ -1,17 +1,12 @@
 const serverless = require("serverless-http");
-const dotenv = require("dotenv");
 const app = require("./app");
-const { Connection } = require("./connection");
+const getDbConnection = require("./connection");
 
 process.on("unCaughtException", () => {
   console.warn("unCaughtException: Shutting down gracefully");
   process.exit(1);
 });
 
-dotenv.config(".env");
-
-const connection_uri = process.env.MONGO_URI;
-
-Connection(connection_uri);
+getDbConnection;
 
 exports.product = serverless(app);
