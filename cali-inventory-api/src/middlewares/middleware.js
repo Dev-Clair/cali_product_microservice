2310e89e2693a4cbac69f65a40f89e8cbeec23a8
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-02 09:56:35 +0100

    feat: implement middlewares

---
+++ cali-inventory-api/src/middlewares/middleware.js
@@ -0,0 +1,27 @@
+const checkRequestContentType = (req, res, next) => {
+  const get_content_type = req.get("Content-Type");
+
+  if (get_content_type !== "application/json") {
+    return res.status(400).json({
+      message: "Invalid content type",
+      expected: "application/json",
+      received: `${get_content_type}`,
+    });
+  }
+
+  next();
+};
+
+const checkRequestSecurity = () => {
+  const get_protocol = req.protocol;
+
+  const get_security = req.secure;
+
+  if (get_protocol !== "https" || get_security === false) {
+    return res.status(403).json({ message: "Connection is not secure." });
+  }
+
+  next();
+};
+
+module.exports = { checkRequestContentType, checkRequestSecurity };
