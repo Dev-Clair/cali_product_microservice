aa1078ad3d1ea643372232b49fb09d398f4089fe
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-06 15:46:09 +0100

    fix: remove start server commented block

--- cali-inventory-api/handler.js
+++ cali-inventory-api/handler.js
@@ -4,8 +4,4 @@ const getDbConnection = require("./connection");
 
 getDbConnection;
 
-// app.listen(3999, () => {
-//   console.log(`Server process started, listening on port 3999`);
-// });
-
 exports.inventory = serverless(app);
