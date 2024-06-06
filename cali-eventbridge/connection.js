e7cdf12914260cd0cac3a656496594a375c53422
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-06 12:59:09 +0100

    feat: provide database connection

---
+++ cali-eventbridge/connection.js
@@ -0,0 +1,53 @@
+const dotenv = require("dotenv");
+const DbConnection = require("mongoose");
+
+dotenv.config();
+
+let currentRetries = 0;
+
+let retryDelay = 5000;
+
+const maxRetries = 5;
+
+const Connect = async () => {
+  const connectionUri = process.env.MONGO_URI;
+  try {
+    await DbConnection.connect(connectionUri, {
+      serverSelectionTimeoutMS: 10000,
+    });
+  } catch (err) {
+    if (currentRetries < maxRetries) {
+      console.log(`Attempting reconnection to database`);
+
+      currentRetries++;
+
+      retryDelay *= 2;
+
+      setTimeout(Connect, retryDelay);
+    } else {
+      console.log(
+        `Database connection error\nMax retries reached, Could not establish connection to database:\n${err.message}`
+      );
+    }
+  }
+};
+
+DbConnection.connection.on("connecting", () => {
+  console.log(`Attempting connection to database`);
+});
+
+DbConnection.connection.on("connected", () => {
+  console.log("Database connection successful");
+});
+
+DbConnection.connection.on("disconnected", () => {
+  console.log("Database connection failure");
+});
+
+DbConnection.connection.on("reconnected", () => {
+  console.log("Database reconnection successful");
+});
+
+Connect();
+
+module.exports = DbConnection;
