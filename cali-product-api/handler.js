0307ce3c43060e972093f154e16e5a52dbc85dd7
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-06 15:44:14 +0100

    fix: remove unCaught exception listener

--- cali-product-api/handler.js
+++ cali-product-api/handler.js
@@ -2,11 +2,6 @@ const serverless = require("serverless-http");
 const app = require("./app");
 const getDbConnection = require("./connection");
 
-process.on("unCaughtException", () => {
-  console.warn("unCaughtException: Shutting down gracefully");
-  process.exit(1);
-});
-
 getDbConnection;
 
 // app.listen(3999, () => {
