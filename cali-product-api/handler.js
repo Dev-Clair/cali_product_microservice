e29ee411809c56cae2dfcdf182311c4e236def51
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-11 22:37:20 +0100

    feat: properly load env variables

--- cali-product-api/handler.js
+++ cali-product-api/handler.js
@@ -1,7 +1,16 @@
 const serverless = require("serverless-http");
 const App = require("./app");
-const GetDbConnection = require("./connection");
+const Config = require("./config");
+const GetConnection = require("./connection");
 
-GetDbConnection;
+GetConnection(Config.MONGO_URI);
 
-exports.product = serverless(App);
+if (Config.NODE_ENV === "development") {
+  App.listen(Config.PORT || 4000, () => {
+    console.log(
+      `Server process started, listening on port ${Config.PORT || 4000}`
+    );
+  });
+} else {
+  exports.product = serverless(App);
+}
